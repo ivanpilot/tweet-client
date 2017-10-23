d0dd8ad067a7b510e7de4684bb21ad0b4842758f
Ivan Pilot <ivanpilot@gmail.com>
2017-10-23 23:14:27 +0200

    design the state at RootPage component and implement a fake state as a start

--- src/components/RootPage.js
+++ src/components/RootPage.js
@@ -4,6 +4,55 @@ import TweetContainer from './TweetContainer';
 
 
 class RootPage extends React.Component {
+
+  state = {
+    activeThreadId: 'user-v1',
+    threads: [
+      {
+        id: 'user-v1',
+        name: 'My Tweets',
+        tweets: [
+          {
+            id: 1,
+            title: "First tweet",
+            description: "This is the first tweet",
+            user_id: 1
+          },
+          {
+            id: 2,
+            title: "Second tweet",
+            description: "This is the second tweet",
+            user_id: 1
+          },
+        ]
+      },
+      {
+        id: 'all',
+        name: 'Wall',
+        tweets: [
+          {
+            id: 1,
+            title: "First tweet",
+            description: "This is the first tweet",
+            user_id: 1
+          },
+          {
+            id: 2,
+            title: "Second tweet",
+            description: "This is the second tweet",
+            user_id: 1
+          },
+          {
+            id: 3,
+            title: "Third tweet",
+            description: "This is the third tweet",
+            user_id: 2
+          },
+        ]
+      }
+    ]
+  }
+
   render(){
     return (
       <div className='ui two column stackable divided grid'>
@@ -12,7 +61,10 @@ class RootPage extends React.Component {
             <VerticalMenu />
           </div>
           <div className='ui twelve wide column'>
-            <TweetContainer />
+            <TweetContainer
+              activeThreadId={this.state.activeThreadId},
+              threads={this.state.threads}
+            />
           </div>
         </div>
       </div>
