24fa972f0b04ff951c5b1a6eb6b46c46da3df344
Ivan Pilot <ivanpilot@gmail.com>
2017-10-24 03:42:26 +0200

    add icon on tweet card to edit and delete tweets

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,13 +2,20 @@ import React from 'react';
 
 class Tweet extends React.Component {
   render () {
-
     const tweets = this.props.tweets.map( (tweet, index) => (
       <div key={index} className="ui cards">
         <div className="card">
           <div className="content">
             <div className="header">{tweet.title}</div>
             <div className="description">{tweet.description}</div>
+            <div className='extra content'>
+            <span className='right floated trash icon' onClick={this.handleTrashClick}>
+              <i className='trash icon' />
+            </span>
+            <span className='right floated edit icon' onClick={this.props.onEditClick}>
+              <i className='edit icon' />
+            </span>
+          </div>
           </div>
         </div>
       </div>
