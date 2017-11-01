ee8e1eacd8c2c942edd73c12f79726faa38c64e2
Ivan Pilot <ivanpilot@gmail.com>
2017-11-01 02:57:38 +0100

    add a better 404 page not found

--- src/components/BodyContainer.js
+++ src/components/BodyContainer.js
@@ -1,17 +1,22 @@
 //will have router and routes to select the page like login / signup / root
 
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { RootPage } from './RootPage';
 import Login from './Login';
+import { NotFound } from './NotFound';
 import '../styles/BodyContainer.css';
 
 class BodyContainer extends React.Component {
   render(){
     return(
       <div className="body-container">
-        <Route exact={true} path='/' component={RootPage} />
-        <Route path='/login' component={Login} />
+        <Switch>
+          <Route exact={true} path='/' component={RootPage} />
+          <Route path='/login' component={Login} />
+          <Route component={NotFound} />
+
+        </Switch>
       </div>
     )
   }
---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/NotFound.css';
+
+export const NotFound = (props) => (
+  <div className='not_found'>
+    <div className='not_found_message'>
+      <h1>404</h1>
+      <h3>Houston, we have a problem!</h3>
+    </div>
+  </div>
+)
