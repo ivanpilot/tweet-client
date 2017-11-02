93606838a0e44f746b2f1d0771c2444a25324db4
Ivan Pilot <ivanpilot@gmail.com>
2017-11-02 14:16:01 +0100

    clean ThreadTabs container component

--- src/components/ThreadTabs.js
+++ src/components/ThreadTabs.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import { offEditableTweetMode } from '../actions/EditableTweet';
 import { openThread } from '../actions/Thread';
-// import { store } from '../store'
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Tabs } from './Tabs';
-
-
-// handleClickTab = (threadId) => (
-//    {
-//   offEditableTweetMode(),
-//   openThread(threadId)
-// })
 
 const MapStateToTabsProps = (state) => {
   const tabs = state.threads.map(t => (
@@ -27,15 +19,6 @@ const MapStateToTabsProps = (state) => {
   }
 }
 
-// const MapDispatchToTabsProps = (dispatch, threadId) => {
-//   // debugger
-//   return(
-//     bindActionCreators({
-//       offEditableTweetMode: offEditableTweetMode(),
-//       openThread: openThread(threadId)
-//     }, dispatch)
-//   )
-// }
 const MapDispatchToTabsProps = (dispatch) => (
   {
     handleClickTab: (threadId) => (
@@ -45,37 +28,17 @@ const MapDispatchToTabsProps = (dispatch) => (
   }
 )
 
-export const ThreadTabs = connect(MapStateToTabsProps, MapDispatchToTabsProps)(Tabs)
+export const ThreadTabs = connect(
+  MapStateToTabsProps,
+  MapDispatchToTabsProps
+)(Tabs)
 
-// class ThreadTabs extends React.Component {
-//
-//   componentDidMount(){
-//     store.subscribe(() => this.forceUpdate())
-//   }
-//
-//   handleClickTab = (threadId) => {
-//     store.dispatch(offEditableTweetMode())
-//     store.dispatch(openThread(threadId))
-//   }
-//
-//   render(){
-//     const state = store.getState()
-//     const activeThreadId = state.activeThreadId
-//     const tabs = state.threads.map(t => (
-//       {
-//         id: t.id,
-//         name: t.name,
-//         active: t.id === activeThreadId
-//       }
-//     ))
-//
-//     return(
-//       <Tabs
-//         tabs={tabs}
-//         handleClickTab={this.handleClickTab}
-//       />
-//     )
-//   }
+// const MapDispatchToTabsProps = (dispatch, threadId) => {
+//   // debugger
+//   return(
+//     bindActionCreators({
+//       offEditableTweetMode: offEditableTweetMode(),
+//       openThread: openThread(threadId)
+//     }, dispatch)
+//   )
 // }
-//
-// export default ThreadTabs
