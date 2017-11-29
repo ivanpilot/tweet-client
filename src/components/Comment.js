7f98d5507502359b67d9f7fc2f3856a7640ea054
Ivan Pilot <ivanpilot@gmail.com>
2017-11-29 14:20:04 +0100

    add the isFetching displaying for comment component

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,31 +2,44 @@ import React from 'react';
 import '../styles/Comment.css'
 
 export const Comment = (props) => {
-  return (
-    <div className="ui cards border-blue">
-      <div className="card ">
-        <div className="content comment-blue">
-          <div className="description ">{props.comment.description}</div>
-          {
-            (props.comment.ownership) ? (
-              <div className='extra content'>
-                <span
-                  className='right floated trash icon'
-                  onClick={() => props.onTrashClick(props.comment.id, props.activeTweet)}
-                >
-                  <a><i className='trash icon' /></a>
-                </span>
-                <span
-                  className='right floated edit icon'
-                  onClick={() => props.onEditClick(props.comment.id, props.editableComment)}
-                >
-                  <a><i className='edit icon' /></a>
-                </span>
-              </div>
-            ) : (null)
-          }
+  if(props.comment.isFetching){
+    return (
+      <div className="ui cards">
+        <div className="card">
+          <div className="content comment-saving">
+            ... saving ...
+            <div className="description ">{props.comment.description}</div>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className="ui cards">
+        <div className="card">
+          <div className="content comment-blue">
+            <div className="description ">{props.comment.description}</div>
+            {
+              (props.comment.ownership) ? (
+                <div className='extra content'>
+                  <span
+                    className='right floated trash icon'
+                    onClick={() => props.onTrashClick(props.comment.id, props.activeTweet)}
+                  >
+                    <a><i className='trash icon' /></a>
+                  </span>
+                  <span
+                    className='right floated edit icon'
+                    onClick={() => props.onEditClick(props.comment.id, props.editableComment)}
+                  >
+                    <a><i className='edit icon' /></a>
+                  </span>
+                </div>
+              ) : (null)
+            }
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
